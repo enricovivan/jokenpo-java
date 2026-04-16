1f1a6a6717c26a39a6cda116534ffcc9283c9f6f
Enrico Vivan <enricovivan21@gmail.com>
2026-04-16 18:12:05 -0300

    first upload

---
+++ src/test/java/br/com/klincloud/jokenpo/JokenpoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.klincloud.jokenpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokenpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
